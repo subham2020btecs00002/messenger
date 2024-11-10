19cd4a559d2dc4c52be2689299f17551770f3de1
subham2020btecs00002 <subham.kumar@in.ncs-i.com>
2024-11-10 10:56:58 +0530

    registration, authentication and role based access done

--- user-service/src/main/java/com/example/userservice/entity/AuthRequest.java
+++ user-service/src/main/java/com/example/userservice/entity/AuthRequest.java
@@ -1,5 +1,15 @@
 package com.example.userservice.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRequest {
+
+    private String username;
+    private String password;
 
 }
--- user-service/src/main/java/com/example/userservice/repository/UserInfoRepository.java
+++ user-service/src/main/java/com/example/userservice/repository/UserInfoRepository.java
@@ -1,5 +1,12 @@
 package com.example.userservice.repository;
 
-public class UserInfoRepository {
+import com.example.userservice.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
 }
